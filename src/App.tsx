74f2c7e35d1a19198533ed08412b7cef3bcf7f1a
Darren McElligott <darren.mcelligott@gmail.com>
2020-11-15 23:22:35 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useRef } from 'react';
 import './App.css';
+import { Activity } from '../../strava-viewer/src/types/activity';
 
 function App() {
+  const map = useRef<google.maps.Map>()
+
+  useEffect(() => {
+    map.current = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+      center: { lat: 30, lng: -100 },
+      zoom: 15
+    });
+  }, []);
+
+  const loadData = useCallback((activityId) => {
+    fetch(`http://localhost:8080/activity/${activityId}`)
+      .then((res) => res.json())
+      .then((activityData: Activity) => {
+
+        if (!map.current) {
+          return;
+        }
+
+        const positionalTracks = activityData.track.filter(({ position }) => !!position)
+
+        const path = positionalTracks.map(({ position }) => position!);
+
+        const routePolyLine = new google.maps.Polyline({
+          path,
+          geodesic: true,
+          strokeColor: "#FF0000",
+          strokeOpacity: 1.0,
+          strokeWeight: 3,
+        });
+
+        routePolyLine.setMap(map.current);
+
+        const firstTrackWithLocation = activityData.track.find(({ position }) => !!position);
+
+        firstTrackWithLocation && map.current && map.current.panTo(firstTrackWithLocation.position!);
+
+
+
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="map-container">
+        <div id="map" style={{ height: '100%' }}>
+        </div>
+      </div>
+      <button onClick={() => loadData('test')}>Load Data</button>
     </div>
   );
 }
